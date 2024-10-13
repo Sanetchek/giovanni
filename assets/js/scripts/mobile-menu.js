7e4ebf65374c01ad550b888a7c079e5e44acbd51
spritesua <spritesreal@gmail.com>
2024-10-13 23:44:19 +0300

    Header menu mobile - added dropdown

---
+++ assets/js/scripts/mobile-menu.js
@@ -0,0 +1,148 @@
+'use strict';
+
+(function ($) {
+ 
+  /**
+   * Header menu mobile only parents - dropdown after click on the parent menu
+   */
+   var isMenuOpen = false;
+
+   function isMobile() {
+       return window.matchMedia('(max-width: 768px)').matches;
+   }
+
+   //menu about
+   $('.mobile-about').click(function(event) {
+    if (isMobile() && !isMenuOpen) {
+        event.preventDefault(); 
+        $('.mega-menu').removeClass('is-show');
+        var $menu = $('#about_menu_mobile');
+
+        $menu.css({
+            'display': 'block',
+            'right': '-100%'
+        });
+
+        $menu.animate({
+            right: '0' 
+        }, 300, function() {
+            isMenuOpen = true; 
+        });
+    }
+    });
+
+   //menu charms
+   $('.mobile-charms').click(function(event) {
+    if (isMobile() && !isMenuOpen) {
+        event.preventDefault(); 
+        $('.mega-menu').removeClass('is-show');
+        var $menu = $('#charms_menu_mobile');
+
+        $menu.css({
+            'display': 'block',
+            'right': '-100%'
+        });
+
+        $menu.animate({
+            right: '0' 
+        }, 300, function() {
+            isMenuOpen = true; 
+        });
+    }
+    });
+
+    //menu collections
+    $('.mobile-collections').click(function(event) {
+     if (isMobile() && !isMenuOpen) {
+         event.preventDefault(); 
+         $('.mega-menu').removeClass('is-show');
+         var $menu = $('#collections_menu_mobile');
+ 
+         $menu.css({
+             'display': 'block',
+             'right': '-100%'
+         });
+ 
+         $menu.animate({
+             right: '0' 
+         }, 300, function() {
+             isMenuOpen = true; 
+         });
+     }
+     });
+
+     //menu jewellery
+     $('.mobile-jewellery').click(function(event) {
+      if (isMobile() && !isMenuOpen) {
+          event.preventDefault(); 
+          $('.mega-menu').removeClass('is-show');
+          var $menu = $('#jewellery_menu_mobile');
+  
+          $menu.css({
+              'display': 'block',
+              'right': '-100%'
+          });
+  
+          $menu.animate({
+              right: '0' 
+          }, 300, function() {
+              isMenuOpen = true; 
+          });
+      }
+      });
+
+      //menu gifts
+      $('.mobile-gifts').click(function(event) {
+       if (isMobile() && !isMenuOpen) {
+           event.preventDefault(); 
+           $('.mega-menu').removeClass('is-show');
+           var $menu = $('#gifts_menu_mobile');
+   
+           $menu.css({
+               'display': 'block',
+               'right': '-100%'
+           });
+   
+           $menu.animate({
+               right: '0' 
+           }, 300, function() {
+               isMenuOpen = true; 
+           });
+       }
+       });
+
+       //menu accessories
+       $('.mobile-accessories').click(function(event) {
+        if (isMobile() && !isMenuOpen) {
+            event.preventDefault(); 
+            $('.mega-menu').removeClass('is-show');
+            var $menu = $('#accessories_menu_mobile');
+    
+            $menu.css({
+                'display': 'block',
+                'right': '-100%'
+            });
+    
+            $menu.animate({
+                right: '0' 
+            }, 300, function() {
+                isMenuOpen = true; 
+            });
+        }
+        });
+
+   // Close menu after click on .mega-menu-header
+   $('.mega-menu-header').click(function() {
+    if (isMobile() && isMenuOpen) {
+        var $menu = $(this).parent().parent();
+
+        $menu.animate({
+            right: '-100%' 
+        }, 300, function() {
+            $menu.css('display', 'none'); 
+            isMenuOpen = false; 
+        });
+    }
+    });
+
+}(jQuery));
--- template-parts/mega-menu/gifts.php
+++ template-parts/mega-menu/gifts.php
@@ -40,3 +40,49 @@
     </div>
   </div>
 </div>
+
+
+<div id="gifts_menu_mobile" class="mobile-gifts mobile-dropdown-menu">
+
+  <div class="page-mob">
+    <button class="mega-menu-header">
+      <?= __('מתנות', 'giovanni') ?>
+    </button>
+  </div>
+
+  <div class="mega-menu-container">
+    <div class="menu-wrap">
+      <div class="menu-1">
+        <p class="menu-title"><?= __('מתנות עבור', 'giovanni') ?></p>
+
+        <?php
+          wp_nav_menu([
+            'theme_location' => 'gifts',
+            'menu_id'        => 'gifts-menu',
+            'container'			 => ''
+          ]);
+        ?>
+      </div>
+
+      <div class="menu-2">
+        <p class="menu-title"><?= __('מקרים', 'giovanni') ?></p>
+
+        <?php
+          wp_nav_menu([
+            'theme_location' => 'occasions',
+            'menu_id'        => 'occasions-menu',
+            'container'			 => ''
+          ]);
+        ?>
+      </div>
+    </div>
+
+    <div class="links-wrap mobile-links-wrap">
+    <?php
+      $links = get_field('menu_gifts', 'options');
+      get_template_part('template-parts/mega-menu/links-mobile', '', ['links' => $links]);
+    ?>
+  </div>
+  </div>
+
+</div>
