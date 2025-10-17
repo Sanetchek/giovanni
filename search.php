1ae0dde43f104d2c948df4fd995a300a5d25c8ce
Andrew <andrishzakdu@gmail.com>
2025-10-17 18:22:13 +0300

    Bugfix, bugfix search

--- search.php
+++ search.php
@@ -2,71 +2,89 @@
 /**
  * The template for displaying search results pages
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
  * @package giovanni
  */
 
 get_header();
 
-// Prepare WooCommerce search query
+$per_page = 24; 
+$paged    = max( 1, get_query_var('paged') );
+
 $args = array(
-	'post_type'      => 'product',
-	's'              => get_search_query(),
-	'posts_per_page' => -1,
+    'post_type'           => 'product',
+    's'                   => get_search_query(),
+    'post_status'         => 'publish',
+    'ignore_sticky_posts' => true,
+    'paged'               => $paged,
+    'posts_per_page'      => $per_page,
+    'no_found_rows'       => false,
 );
 
-$query = new WP_Query($args);
-
-// Get the total number of posts
+$query      = new WP_Query($args);
 $post_count = $query->found_posts;
 ?>
 
-	<main id="primary" class="site-main search-page">
-		<div class="container">
-			<header class="page-header">
-				<h1 class="page-title main-title search-title">
-					<?= __('תוצאות חיפוש', 'giovanni'); ?>
-				</h1>
-				<p class="search-result-count">
-					<?php
-						/* translators: %s: search query. */
-						echo $post_count . __( ' תוצאות עבור ', 'giovanni' ) . '<span class="search-keywords">' . get_search_query() . '</span>';
-					?>
-				</p>
-			</header><!-- .page-header -->
-		</div>
+<main id="primary" class="site-main search-page">
+    <div class="container">
+        <header class="page-header">
+            <h1 class="page-title main-title search-title">
+                <?= __('תוצאות חיפוש', 'giovanni'); ?>
+            </h1>
+            <p class="search-result-count">
+                <?= $post_count . __( ' תוצאות עבור ', 'giovanni' ) . '<span class="search-keywords">' . get_search_query() . '</span>'; ?>
+            </p>
+        </header>
+    </div>
 
-		<div id="products-filter" class="products-filter">
-			<div class="filter-mob">
-				<button type="button" class="btn btn-no-border open-filter">
-					<span class="filter-label"><?= __('מסנן', 'giovanni') ?></span>
-					<svg class='icon-filter' width='24' height='24'>
-						<use href='<?= assets('img/sprite.svg#icon-filter') ?>'></use>
-					</svg>
-				</button>
-			</div>
+    <div id="products-filter" class="products-filter">
+        <div class="filter-mob">
+            <button type="button" class="btn btn-no-border open-filter">
+                <span class="filter-label"><?= __('מסנן', 'giovanni') ?></span>
+                <svg class='icon-filter' width='24' height='24'>
+                    <use href='<?= assets('img/sprite.svg#icon-filter') ?>'></use>
+                </svg>
+            </button>
+        </div>
 
-			<div class="products-container">
-				<?php display_product_filters(get_search_query()); ?>
-			</div>
-		</div>
+        <div class="products-container">
+            <?php display_product_filters(get_search_query()); ?>
+        </div>
+    </div>
 
-		<ul id="product-list" class="search-modal-content">
-			<?php
-				if ($query->have_posts()) {
-					while ($query->have_posts()) {
-						$query->the_post();
+    <ul id="product-list" class="search-modal-content">
+        <?php
+        if ($query->have_posts()) {
+            while ($query->have_posts()) {
+                $query->the_post();
+                wc_get_template_part('content', 'product');
+            }
+        } else {
+            echo '<li class="results-not-found">' . __('לא נמצאו תוצאות', 'giovanni') . '</li>';
+        }
+        ?>
+    </ul>
 
-						wc_get_template_part( 'content', 'product' );
-					}
-				} else {
-					echo '<li class="results-not-found">' . __('לא נמצאו תוצאות', 'giovanni') . '</li>';
-				}
-			?>
-		</ul>
+    <?php
+    $big   = 999999999;
+    $paged = max(1, get_query_var('paged'));
 
-	</main><!-- #main -->
+    $links = paginate_links(array(
+        'base'      => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+        'format'    => '?paged=%#%',
+        'total'     => $query->max_num_pages,
+        'current'   => $paged,
+        'prev_text' => '‹',
+        'next_text' => '›',
+        'type'      => 'list',
+    ));
 
-<?php
-get_footer();
+    if ($links) {
+        echo '<nav class="pagination-wrap" aria-label="Search results pages">' . $links . '</nav>';
+    }
+
+    wp_reset_postdata();
+    ?>
+
+</main>
+
+<?php get_footer(); ?>
